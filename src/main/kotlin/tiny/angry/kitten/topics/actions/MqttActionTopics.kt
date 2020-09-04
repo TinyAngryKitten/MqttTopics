7bef909b0d1f12c56f1245a503b2fdd0f6cf72dd
sander høyvik <sander.hoyvik@gmail.com>
2020-09-04 07:26:06 +0200

    Update MqttActionTopics.kt

--- src/main/kotlin/tiny/angry/kitten/topics/actions/MqttActionTopics.kt
+++ src/main/kotlin/tiny/angry/kitten/topics/actions/MqttActionTopics.kt
@@ -2,5 +2,6 @@ package tiny.angry.kitten.topics.actions
 
 object MqttActionTopics {
     val topic = "action/+"
+
     fun createTopicWithAction(actionName : String) = topic.replace("+",actionName)
 }
