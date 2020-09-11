7d89fc94fff06d1c52fb8c716f11ea667a41f764
sander høyvik <sander.hoyvik@gmail.com>
2020-09-11 23:35:39 +0200

    hue hub

---
+++ src/main/kotlin/tiny/angry/kitten/topics/harmony/HarmonyHubActions.kt
@@ -0,0 +1,88 @@
+package tiny.angry.kitten.topics.harmony
+
+enum class HarmonyHubActions {
+    VolumeDown,
+    VolumeUp,
+
+    ChannelUp,
+    ChannelDown,
+
+    Netflix,
+
+    InputHdmi4,
+    InputHdmi3,
+    InputHdmi2,
+    InputHdmi1,
+
+    //Home Appliance functions
+    Oscillation,
+    Speed,
+    Mode,
+    Timer,
+
+    PowerOff,
+    PowerOn,
+    PowerToggle,//use toggle for home appliances
+
+    Mute,
+
+    DirectionDown,
+    DirectionUp,
+    DirectionLeft,
+    DirectionRight,
+    OK,
+    SELECT {
+        override fun toString() = "OK"
+    },
+
+    Zero {
+        override fun toString(): String {
+            return "0"
+        }
+    },
+    One {
+        override fun toString(): String {
+            return "1"
+        }
+    },
+    Two {
+        override fun toString(): String {
+            return "2"
+        }
+    },
+    Three {
+        override fun toString(): String {
+            return "3"
+        }
+    },
+    Four {
+        override fun toString(): String {
+            return "4"
+        }
+    },
+    Five {
+        override fun toString(): String {
+            return "5"
+        }
+    },
+    Six {
+        override fun toString(): String {
+            return "6"
+        }
+    },
+    Seven {
+        override fun toString(): String {
+            return "7"
+        }
+    },
+    Eight {
+        override fun toString(): String {
+            return "8"
+        }
+    },
+    Nine {
+        override fun toString(): String {
+            return "9"
+        }
+    }
+}
---
+++ src/main/kotlin/tiny/angry/kitten/topics/harmony/HarmonyHubTopic.kt
@@ -0,0 +1,9 @@
+package tiny.angry.kitten.topics.harmony
+
+object HarmonyHubTopic {
+    val topic = "harmony/+/+"
+
+    fun createTopicWith(hubName : String = "livingroom", device : String = "+") = "harmony/$hubName/$device"
+
+    fun createPayload(action : HarmonyHubActions) = action.toString()
+}
