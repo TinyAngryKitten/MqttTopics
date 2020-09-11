9260e5c748844d22ed74a64a9e93f5a4ef5bc98b
sander høyvik <sander.hoyvik@gmail.com>
2020-09-11 23:36:04 +0200

    Lol game notifications

--- src/main/kotlin/tiny/angry/kitten/topics/livegamenotification/LoLGameNotification.kt
+++ src/main/kotlin/tiny/angry/kitten/topics/livegamenotification/LoLGameNotification.kt
@@ -1,2 +1,7 @@
 package tiny.angry.kitten.topics.livegamenotification
 
+object LoLGameNotification {
+    val topic = "game/league/+"
+
+    fun createTopicForUser(username : String) = topic.replace("+",username)
+}
